0ae01d6bcbf466550789e0d4e36d144929eebf89
mahmoud778899 <mhoda8952@gmail.com>
2025-06-10 21:13:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 fetch('https://api.exchangerate.host/latest?base=USD')
   .then(response => {
-    if (!response.ok) throw new Error("Network error");
-    return response.json();
-  })
   .then(data => {
     const rate = data.rates.EGP;
     document.getElementById("price").textContent = `1 USD = ${rate.toFixed(2)} EGP`;
